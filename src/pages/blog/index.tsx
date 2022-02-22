e252739b215550689af6e8b1ecc98f691f81d5f9
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2022-02-23 02:14:19 +0900

    blogページを追加

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from '~/components/Head'
+
+const IndexBlog: NextPage = () => {
+  return (
+    <>
+      <Head title="Blog Top" />
+
+      <section>
+        <h1>記事一覧</h1>
+      </section>
+    </>
+  )
+}
+
+export default IndexBlog
