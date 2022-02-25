20bf997489b1aa30c630258164b198d3f92b3261
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2022-02-26 02:01:20 +0900

    aboutページの追加

---
+++ src/pages/about.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from 'next'
+import Head from '~/components/Head'
+
+const About: NextPage = () => {
+  return (
+    <>
+      <Head title="about" />
+
+      <h1>About</h1>
+      <div>私は金沢工業大学の学生です。</div>
+    </>
+  )
+}
+
+export default About
